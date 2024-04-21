bade7e2ff1a930ca04b3a8b8b26d4452d35fe69c
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2024-04-21 20:37:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
      <h1 className='text-3xl font-medium text-red-500'>This is Home page</h1>
+      <h2 className='font-small'>this is hello</h2>
     </>
   )
 }
