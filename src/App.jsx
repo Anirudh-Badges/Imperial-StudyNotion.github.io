71c5d947005a5fe57e01662a930f9ed10a33933a
AnirudhSharmagithub01 <anirudhsharma857000@gmail.com>
2024-04-21 20:39:31 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
      <h1 className='text-3xl font-medium text-red-500'>This is Home page</h1>
+     <h2 className='text-2xl font-bold'>Hello world</h2>
     </>
   )
 }
