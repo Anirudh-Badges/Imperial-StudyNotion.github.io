9aeed41085b60f39fa99932a6df6734b670e89e4
AnirudhSharmagithub01 <anirudhsharma857000@gmail.com>
2024-04-22 19:00:08 +0530

    code write

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaArrowRight } from "react-icons/fa";
 
 const Homepage = () => {
   return (
     <div>
-      This is Homepage
+      
+      <div className='w-11/12 mx-auto mt-16 transtion-all duration-200'>
+        <Link to={'/signup'}>
+            <div className=''>
+                <div className='bg-richblack-400 w-fit flex justify-center items-center gap-2 rounded-full p-[6px]'>
+                    <p className='text-white'>Become an Instructor</p>
+                    <FaArrowRight/>
+                </div>
+            </div>
+        </Link>
+      </div>
     </div>
   )
 }
