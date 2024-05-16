94fba6077c830ee2a2312b118627f7c4a2f8ec73
Amisha73 <08amisha2003@gmail.com>
2024-05-16 15:11:05 +0530

    done

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,6 +6,7 @@ import about3 from "../assets/Images/aboutus3.webp";
 import foundingStory from "../assets/Images/FoundingStory.png";
 import Footer from "../Components/Comman/Footer";
 import GetInTouchForm from "../Components/cores/About/GetInTouchForm";
+import GridSection from "../Components/cores/About/GridSection";
 
 
 const visitors = [
@@ -126,7 +127,7 @@ const AboutPage = () => {
       {/* section-5  */}
       <section className="bg-richblack-800 h-[150px] mb-5">
         <div className="w-11/12 mx-auto max-w-maxContent flex items-center justify-center gap-10 text-richblack-500">
-            <div className="flex items-center justify-between gap-24 lg:flex-row flex-col">
+            <div className="flex items-center justify-between gap-32 lg:flex-row flex-col">
                 {
                     visitors.map((ele,i) => {
                         return(
@@ -140,6 +141,13 @@ const AboutPage = () => {
             </div>
         </div>
       </section>
+
+      <section>
+        <div className=" mt-20 mb-10">
+          <GridSection/>
+        </div>
+      </section>
+      
       {/* GetinTouch  */}
       <GetInTouchForm
         title={"Get In Touch"}
